ff402d95bb930257d3cff3cb08fe30f714068d3d
Peter DiSalvo <pdisalvo486@gmail.com>
2018-09-29 22:10:51 -0500

    Restructure project into modules

---
+++ tictactoeui/src/main/java/com/disalvo/peter/App.java
@@ -0,0 +1,13 @@
+package com.disalvo.peter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
